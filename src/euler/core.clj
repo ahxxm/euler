92d983876a32d968d48ee91f1e13b5506f565266
ahxxm <i@ahxxm.com>
2021-04-12 19:48:26 +0800

    59

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1162,6 +1162,39 @@
         (recur (+ sl 2) (+ z sl) p)))))
 
 
+(defn pad
+  [key n]
+  (clojure.string/join
+   (take n (repeat key))))
+
+(defn xor
+  ;; key string, msg as int array
+  [-key msg]
+  (let [kk (pad -key (count msg))
+        r   (mapv bit-xor (->> kk vec (map int)) msg )]
+    (apply str (map char r))))
+
+;; key be "exp"
+;; An extract taken from the introduction of one of Euler's most celebrated papers, "De summis serierum reciprocarum" [On the sums of series of reciprocals]: I have recently found, quite unexpectedly, an elegant expression for the entire sum of this series 1 + 1/4 + 1/9 + 1/16 + etc., which depends on the quadrature of the circle, so that if the true sum of this series is obtained, from it at once the quadrature of the circle follows. Namely, I have found that the sum of this series is a sixth part of the square of the perimeter of the circle whose diameteris 1; or by putting the sum of this series equal to s, it has the ratio sqrt(6) multiplied by s to 1 of the perimeter to the diameter. I will soon show that the sum of this series to be approximately 1.644934066842264364; and from multiplying this number by six, and then taking the square root, the number 3.141592653589793238 is indeed produced, which expresses the perimeter of a circle whose diameter is 1. Following again the same steps by which I had arrived at this sum, I have discovered that the sum of the series 1 + 1/16 + 1/81 + 1/256 + 1/625 + etc.
+;; also depends on the quadrature of the circle. Namely, the sum of this multiplied by 90 gives the biquadrate (fourth power) of the circumference of the perimeter of a circle whose diameter is 1. And by similar reasoning I have likewise been able to determine the sums of the subsequent series in which the exponents are even numbers.
+(defn solve-59
+  []
+  (let [cipher (slurp "https://projecteuler.net/project/resources/p059_cipher.txt")
+        splits (clojure.string/split cipher #",")
+        arr    (map #(Integer. %) splits)
+        keys   (for [a (map char (range 97 123))
+                     b (map char (range 97 123))
+                     c (map char (range 97 123))]
+                 [a b c])]
+    (loop [k  (first keys)
+           ks (rest keys)]
+      (if-not (nil? k)
+        (let [kk (apply str k)
+              text (xor kk arr)
+              c (count (re-seq #"the" text))]
+          (if (< c 10) ;; 10 after some test
+            (recur (first ks) (rest ks))
+            (reduce + (map int text))))))))
 
 (defn main
   "I don't do a whole lot."
